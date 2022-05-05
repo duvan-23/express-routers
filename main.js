84e99594033891dc972fbde969cc133b21d0afd9
Duvan Caicedo <duvamendi2@hotmail.com>
2022-05-04 22:40:43 -0500

    entrega desafio

---
+++ main.js
@@ -0,0 +1,153 @@
+const fs =require('fs');
+
+class Contenedor{
+   
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+    async save(data) {    
+        let ruta =`./${this.nombre}`;   
+        
+        return await leer(ruta,data);
+          
+    }
+    async getAll(){
+        let ruta =`./${this.nombre}`;
+        return await leerAll(ruta);
+    }
+    async getById(id){
+        let ruta =`./${this.nombre}`;
+        return await leerId(id,ruta);
+    }
+    async deleteById(id){
+        let ruta =`./${this.nombre}`;
+        return await eliminarId(id,ruta);
+    }
+    async putId(id,datos){
+        let ruta =`./${this.nombre}`;
+        return await actualizar(id,ruta,datos);
+    }
+}
+
+async function actualizar(id,ruta,datos){
+    let resultado2,r3;
+    try{
+        const contenido =await fs.promises.readFile(ruta, 'utf8');
+        resultado2 =JSON.parse(contenido);
+        resultado2.forEach((object,index) =>{
+            if(object.id === Number(id)){
+                r3=index;
+            }
+        });
+        resultado2.splice(r3,1,datos);
+    }
+    catch(err){
+        resultado2= err;
+    }
+    return actualizar2(resultado2,ruta);
+}
+async function actualizar2(resultado2,ruta){
+    try{
+        await fs.promises.writeFile(ruta, JSON.stringify(resultado2,null, 2))
+        console.log("Actualizar");
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+async function eliminarId(id,ruta){
+    let resultado,r2;
+    try{
+        const contenido =await fs.promises.readFile(ruta, 'utf8');
+        resultado =JSON.parse(contenido);
+        resultado.forEach((object,index) =>{
+            if(object.id == id){
+                r2=index;
+            }
+        });
+        resultado.splice(r2,1);
+    }
+    catch(err){
+        resultado= err;
+    }
+    return guardar1(resultado,ruta);
+}
+async function guardar1(resultado,ruta){
+    try{
+        await fs.promises.writeFile(ruta, JSON.stringify(resultado,null, 2))
+        console.log("Elimino");
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+
+
+
+async function leer(ruta,data){
+    let resultado,r2;
+    try{
+        const contenido =await fs.promises.readFile(ruta, 'utf8');
+        resultado =JSON.parse(contenido);
+        r2=resultado;
+        resultado=resultado[resultado.length-1].id+1;
+    }
+    catch(err){
+        r2= [];
+        resultado= 1;
+    }
+    return  guardar([resultado,r2],data,ruta);
+}
+
+async function guardar(resultados,data, ruta){
+    let id_retorna, resultado;
+    let datos =data;
+    const idd=resultados[0];
+    datos.id= resultados[0];
+    resultados[1].push(datos);
+    const datos1= JSON.stringify(resultados[1]);
+    try{
+        await fs.promises.writeFile(ruta, datos1);
+        resultado=2;
+        console.log(idd);
+    }
+    catch(err){
+        resultado=err;
+    }
+    return  " "+idd;
+}
+
+async function leerAll(ruta){
+    let resultado,r2;
+    try{
+        const contenido =await fs.promises.readFile(ruta, 'utf8');
+        resultado =JSON.parse(contenido);
+    }
+    catch(err){
+        resultado= err;
+    }
+    return resultado;
+}
+
+async function leerId(id,ruta){
+    let resultado;
+    try{
+        const contenido =await fs.promises.readFile(ruta, 'utf8');
+        resultado =JSON.parse(contenido);
+        resultado.forEach(object =>{
+            if(object.id ===id){
+                resultado=object;
+            }
+        });
+        if(resultado.length>0){
+            resultado={error:"producto no encontrado"}
+        }
+    }
+    catch(err){
+        resultado= err;
+    }
+    return resultado;
+}
+exports.Contenedor = Contenedor;
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { Router } = express;
+const proyecto = require('./main.js');
+
+
+let contenedor = new proyecto.Contenedor("productos.txt");
+
+const PORT = 8080
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
+const productos = new Router()
+
+productos.use(express.json())
+
+
+productos.get('/',async (req, res) => {
+    
+    res.send(await contenedor.getAll());
+})
+
+productos.get('/:id', async (req, res) => { 
+    const {id} = req.params;
+    res.send(await contenedor.getById(Number(id)));
+})
+
+productos.post('/',async(req, res) => {
+    res.send(await contenedor.save(req.body));
+})
+
+
+productos.delete('/:id',async(req, res) => {
+    const {id} = req.params;
+    res.send(await contenedor.deleteById(Number(id)));
+})
+
+productos.put('/:id', async (req, res) => {
+    const { id } = req.params
+    let datos = req.body;
+    datos.id=id;
+    res.send(await contenedor.putId(id,datos));
+})
+
+app.use('/api/productos', productos)
+
+const server = app.listen(PORT, () => {
+    console.log('Servidor HTTP escuchando en el puerto ' + PORT)
+})
+
